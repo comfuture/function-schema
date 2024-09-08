3bed285c9346e7d3e420b3f058be5f4a9af17c37
comfuture <comfuture@gmail.com>
2024-09-08 08:45:49 +0000

    Implement PEP-0727 Doc in Annotation metadata

---
+++ test/test_pep_0727_doc.py
@@ -0,0 +1,33 @@
+from typing import Annotated
+from enum import Enum
+from function_schema.core import get_function_schema, Doc
+
+
+def test_docs_in_annotation():
+    """Test a function with annotations with Doc"""
+    def func1(a: Annotated[int, Doc("An integer parameter")]):
+        """My function"""
+        ...
+
+    schema = get_function_schema(func1)
+    assert schema["name"] == "func1", "Function name should be func1"
+    assert schema["description"] == "My function", "Function description should be there"
+    assert schema["parameters"]["properties"]["a"]["type"] == "number", "parameter a should be an integer"
+    assert schema["parameters"]["properties"]["a"]["description"] == "An integer parameter", "parameter a should have a description"
+    assert schema["parameters"]["required"] == [
+        "a"], "parameter a should be required"
+
+
+def test_doc_in_nth_args():
+    """Test a function with annotations with Doc in nth args"""
+    def func1(a: Annotated[str, Enum("Candidates", "a b c"), Doc("A string parameter")]):
+        """My function"""
+        ...
+
+    schema = get_function_schema(func1)
+    assert schema["name"] == "func1", "Function name should be func1"
+    assert schema["description"] == "My function", "Function description should be there"
+    assert schema["parameters"]["properties"]["a"]["type"] == "string", "parameter a should be an string"
+    assert schema["parameters"]["properties"]["a"]["description"] == "A string parameter", "parameter a should have a description"
+    assert schema["parameters"]["properties"]["a"]["enum"] == [
+        "a", "b", "c"], "parameter a should have enum values"
