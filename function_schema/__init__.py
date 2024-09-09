ab1434bcb89165bc13e774ccc8c1badead10da60
comfuture <comfuture@gmail.com>
2024-09-09 09:44:17 +0000

    chore: Bump version to 0.4.1

--- function_schema/__init__.py
+++ function_schema/__init__.py
@@ -3,7 +3,7 @@ A small utility to generate JSON schemas for python functions.
 """
 from .core import get_function_schema
 
-__version__ = "0.4.0"
+__version__ = "0.4.1"
 __all__ = (
     "get_function_schema",
     "__version__",
