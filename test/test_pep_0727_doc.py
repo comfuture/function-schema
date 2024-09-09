8c7aefac4312621ed7d73bfc9d205368f58cde86
comfuture <comfuture@gmail.com>
2024-09-09 09:44:03 +0000

    Enhance `Doc` in `Annotation` and implement its tests

--- test/test_pep_0727_doc.py
+++ test/test_pep_0727_doc.py
@@ -31,3 +31,29 @@ def test_doc_in_nth_args():
     assert schema["parameters"]["properties"]["a"]["description"] == "A string parameter", "parameter a should have a description"
     assert schema["parameters"]["properties"]["a"]["enum"] == [
         "a", "b", "c"], "parameter a should have enum values"
+
+
+def test_multiple_docs_in_annotation():
+    """Test a function with annotations with multiple Doc"""
+    def func1(a: Annotated[int, Doc("An integer parameter"), Doc("A number")]):
+        """My function"""
+        ...
+
+    schema = get_function_schema(func1)
+    assert schema["name"] == "func1", "Function name should be func1"
+    assert schema["description"] == "My function", "Function description should be there"
+    assert schema["parameters"]["properties"]["a"]["type"] == "number", "parameter a should be an integer"
+    assert schema["parameters"]["properties"]["a"]["description"] == "An integer parameter", "first description should be used"
+
+
+def test_mixed_docs_in_annotation():
+    """Test a function with annotations with mixed Doc and strings"""
+    def func1(a: Annotated[int, "An integer parameter", Doc("A number")]):
+        """My function"""
+        ...
+
+    schema = get_function_schema(func1)
+    assert schema["name"] == "func1", "Function name should be func1"
+    assert schema["description"] == "My function", "Function description should be there"
+    assert schema["parameters"]["properties"]["a"]["type"] == "number", "parameter a should be an integer"
+    assert schema["parameters"]["properties"]["a"]["description"] == "A number", "`Doc` should be used rather than string"
