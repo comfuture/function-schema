3502f3d4e6435798c0fb214661d29624d4f50bdd
comfuture <comfuture@gmail.com>
2024-11-21 08:45:20 +0900

    version bump

--- function_schema/__init__.py
+++ function_schema/__init__.py
@@ -3,7 +3,7 @@ A small utility to generate JSON schemas for python functions.
 """
 from .core import get_function_schema, guess_type, Doc, Annotated
 
-__version__ = "0.4.4"
+__version__ = "0.4.5"
 __all__ = (
     "__version__",
     "get_function_schema",
